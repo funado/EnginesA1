bade3990ab4080eec75584b77c5a62f2c98dc556
Aaron Chan <jofaduh123@outlook.com>
2021-09-29 12:02:40 -0400

    Final Proto, still WIP

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,21 +6,38 @@ public class Move : MonoBehaviour
 {
     private Rigidbody rb;
     private float movespeed;
-    private float dirX, dirZ, dirY;
+    private float dirX, dirZ;
+    private float jumpStrength;
+
+    private float rotX, rotY = 0.0f;
 
     public GameObject player;
     public float sensitivity;
     public Vector3 jump;
-    private float jumpStrength;
     public bool grounded;
+
+    public float msens = 200.0f;
+    public float clamp = 80.0f;
+
+    public float climbSpeed = 5.0f;
+    public float sticktowall = 1.0f;
+    public float range = 1.0f;
+    public float gravity;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        movespeed = 3.0f;
+        movespeed = 50.0f;
         jumpStrength = 2.0f;
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 1.5f, 0.0f);
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        
+        
     }
 
     // Update is called once per frame
@@ -34,6 +51,20 @@ public class Move : MonoBehaviour
         {
             rb.AddForce(jump * jumpStrength, ForceMode.Impulse);
         }
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotX += mouseY * msens * Time.deltaTime;
+        rotY += mouseX * msens * Time.deltaTime;
+
+
+        rotX = Mathf.Clamp(rotX, -clamp, clamp);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+
+        //WallClimb();
     }
 
     void OnCollisionStay()
@@ -47,6 +78,48 @@ public class Move : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(dirX, rb.velocity.y, dirZ);
+        //rb.velocity = new Vector3(dirX, rb.velocity.y, dirZ);
+        //rb.velocity = rb.velocity * rb.velocity.y;
+        if(Input.GetKey(KeyCode.W))
+        {
+            rb.velocity += transform.forward * Time.deltaTime * 10.0f;
+            rb.velocity += transform.up * Time.deltaTime * gravity;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = -transform.right * Time.deltaTime * movespeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = -transform.forward * Time.deltaTime * movespeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = transform.right * Time.deltaTime * movespeed;
+        }
+    }
+
+    public void WallClimb()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            if (hit.transform.tag == "Wall")
+            {
+                gravity = 1f;
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    print("wc");
+                    //transform.position += transform.forward * Time.deltaTime * sticktowall;
+                    //transform.position += transform.up * Time.deltaTime * climbSpeed;
+                    rb.velocity += transform.up * Time.deltaTime * climbSpeed * 2.0f;
+                }
+            }
+        }
+        else
+        {
+            //print("not wc");
+            gravity = -3.81f;
+        }
     }
 }   
--- Assets/Scripts/animationStateController.cs
+++ Assets/Scripts/animationStateController.cs
@@ -17,14 +17,25 @@ public class animationStateController : MonoBehaviour
     {
 
         bool isRunning = animator.GetBool("isRunning");
-        bool forward = Input.GetKey(KeyCode.W);
-        
+        bool forward = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
+
+        bool isBacking = animator.GetBool("isBacking");
+        bool back = Input.GetKey(KeyCode.S);
+
+        //bool isClimbing = animator.GetBool("isClimbing");
+        //bool wallClimb = Input.GetKey(KeyCode.Space);
+
+        bool isJumping = animator.GetBool("isJumping");
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+
 
         if (forward)
         {
             animator.SetBool("isRunning", true);
             animator.SetBool("isJumping",false);
             animator.SetBool("isIdle", false);
+            
 
         }
         if (!forward && isRunning)
@@ -33,21 +44,62 @@ public class animationStateController : MonoBehaviour
             animator.SetBool("isJumping", false);
             animator.SetBool("isIdle", true);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (forward && jump)
         {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isJumping", true);
+            animator.SetBool("isIdle", false);
+        }
+        if (jump)
+        {
+            print("wallah");
             animator.SetBool("isJumping", true);
             animator.SetBool("isRunning", false);
             animator.SetBool("isIdle", false);
-            print("jump");
+            animator.SetBool("isClimbing", false);
+            animator.SetBool("isBacking", false);
+        }
+        if(!jump && isJumping)
+        {
+            print("not wallah");
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isClimbing", false);
+            animator.SetBool("isBacking", false);
 
         }
-
-        //else
+        if (back)
+        {
+            animator.SetBool("isBacking", true);
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isClimbing", false);
+        }
+        if(!back && isBacking)
+        {
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isBacking", false);
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isClimbing", false);
+        }
+        //if (wallClimb)
         //{
-        //    animator.SetBool("isIdle", true);
+        //    animator.SetBool("isClimbing", true);
+        //    animator.SetBool("isIdle", false);
         //    animator.SetBool("isJumping", false);
         //    animator.SetBool("isRunning", false);
+        //    animator.SetBool("isBacking", false);
         //}
-        //animator.SetBool("isIdle", true);
+        //if (!wallClimb && isClimbing)
+        //{
+        //    animator.SetBool("isIdle", true);
+        //    animator.SetBool("isClimbing", false);
+        //    animator.SetBool("isJumping", false);
+        //    animator.SetBool("isRunning", false);
+        //    animator.SetBool("isBacking", false);
+        //}
     }
 }
